a5f0e949cb8b79f6d19c47c69dd9253560cb963c
manumathewthomas <manumathewthomas@gmail.com>
2018-12-11 16:13:06 -0800

    Project1
    Project1

---
+++ Assets/HoloToolkit/Input/Scripts/Utilities/Interactions/TriggerButton.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEngine;
+
+namespace HoloToolkit.Unity.InputModule
+{
+    /// <summary>
+    /// Very simple class that implements basic logic for a trigger button.
+    /// </summary>
+    public class TriggerButton : MonoBehaviour, IInputHandler
+    {
+
+
+        public CSVParsing smScript;
+        public GameObject step;
+        public GameObject hr;
+        public GameObject week1;
+        public GameObject week2;
+        public GameObject week3;
+
+        public int mode;
+        public int weekID;
+
+        /// <summary>
+        /// Indicates whether the button is clickable or not.
+        /// </summary>
+        [Tooltip("Indicates whether the button is clickable or not.")]
+        public bool IsEnabled = true;
+
+        public event Action ButtonPressed;
+
+
+        /// <summary>
+        /// Press the button programmatically.
+        /// </summary>
+
+        public void Start()
+        {
+            step.GetComponent<TextMesh>().color = Color.red;
+            hr.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+
+            week1.GetComponent<TextMesh>().color = Color.yellow;
+            week2.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+            week3.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+
+        }
+        public void Press()
+        {
+            if (IsEnabled)
+            {
+                ButtonPressed.RaiseEvent();
+            }
+        }
+
+        void IInputHandler.OnInputDown(InputEventData eventData)
+        {
+            // Nothing.
+
+        }
+
+        void IInputHandler.OnInputUp(InputEventData eventData)
+        {
+            if (IsEnabled && eventData.PressType == InteractionSourcePressInfo.Select)
+            {
+                ButtonPressed.RaiseEvent();
+                eventData.Use(); // Mark the event as used, so it doesn't fall through to other handlers.
+
+
+
+                if (eventData.selectedObject.name == "StepParent")
+                {
+                    mode = 0;
+                    step.GetComponent<TextMesh>().color = Color.red;
+                    hr.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+                }
+                else if (eventData.selectedObject.name == "HRParent")
+                {
+                    mode = 1;
+                    step.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+                    hr.GetComponent<TextMesh>().color = Color.red;
+                }
+
+                if (eventData.selectedObject.name == "Week1Parent")
+                {
+                    weekID = 0;
+                    week1.GetComponent<TextMesh>().color = Color.yellow;
+                    week2.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+                    week3.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+                }
+                else if (eventData.selectedObject.name == "Week2Parent")
+                {
+                    weekID = 1;
+                    week1.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+                    week2.GetComponent<TextMesh>().color = Color.yellow;
+                    week3.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+                }
+                else if (eventData.selectedObject.name == "Week3Parent")
+                {
+                    weekID = 2;
+                    week1.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+                    week2.GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+                    week3.GetComponent<TextMesh>().color = Color.yellow;
+                }
+
+               
+                smScript.visualize(mode, weekID);
+
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CSVParsing.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+using System.Collections.Generic;
+
+public class CSVParsing : MonoBehaviour
+{
+    public TextAsset csvFile;
+    public enum weekEnum { Week1=0,Week2=5,Week3=10};
+    public weekEnum week;
+    public GameObject stepPrefab;
+    public GameObject hrPrefab;
+    private int weekSelectedValue;
+
+    
+
+    private char lineSeperater = '\n'; // It defines line seperate character
+    private char fieldSeperator = ','; // It defines field seperate chracter
+
+    private int[] stepScaleOptions = { 5, 8, 11, 15 };
+    private float[] stepYAxisPosition = { 24.7f, 33.7f, 41.5f, 54.2f };
+
+    List<string> dates = new List<string>();
+    List<string> steps = new List<string>();
+    List<string> hr = new List<string>();
+
+    void Start()
+    {
+        readData();
+        visualize(0, 0);
+    }
+
+    private void Update()
+    {
+    }
+    // Read data from CSV file
+    private void readData()
+    {
+        string[] records = csvFile.text.Split(lineSeperater);
+      
+        foreach (string record in records)
+        {
+            string[] fields = record.Split(fieldSeperator);
+            dates.Add(fields[0]);
+            steps.Add(fields[1]);
+            hr.Add(fields[2]);
+        }
+    }
+
+    public void visualize(int mode, int weekID)
+    {
+        Debug.Log(mode);
+        GameObject[] trees;
+        GameObject tempTree;
+        trees = GameObject.FindGameObjectsWithTag("data");
+
+        foreach (GameObject tree in trees)
+        {
+            Destroy(tree);
+        }
+
+        if (weekID == 0)
+            weekSelectedValue = 0;
+        else if (weekID == 1)
+            weekSelectedValue = 5;
+        else
+            weekSelectedValue = 10;
+
+        List<string> tempDates = dates.GetRange(weekSelectedValue, 5);
+        List<string> tempSteps = steps.GetRange(weekSelectedValue, 5);
+        List<string> tempHr = hr.GetRange(weekSelectedValue, 5);
+        for (int i = 0; i < tempDates.Count; i++)
+        {
+          
+            int tempStepScale = 0;
+            float tempStepYAxisPosition = 0;
+            int tempStep = int.Parse(tempSteps[i]);
+            int tempHR_i = int.Parse(tempHr[i]);
+
+            if((tempStep >2000 && tempStep <= 3500 && mode==0) || (tempHR_i > 60 && tempHR_i <= 75 && mode == 1))
+            {
+                tempStepScale = 5;
+                tempStepYAxisPosition = 24.7f;
+
+            }
+            else if ((tempStep > 3500 && tempStep <= 5000 && mode == 0) || (tempHR_i > 75 && tempHR_i <= 90 && mode == 1))
+            {
+                tempStepScale = 8;
+                tempStepYAxisPosition = 33.7f;
+            }
+            else if ((tempStep > 5000 && tempStep <= 6500 && mode == 0) || (tempHR_i > 90 && tempHR_i <= 105 && mode == 1))
+            {
+                tempStepScale = 11;
+                tempStepYAxisPosition = 41.5f;
+            }
+            else if((tempStep > 6500 && mode == 0) || (tempHR_i > 105 && mode == 1))
+            {
+                tempStepScale = 15;
+                tempStepYAxisPosition = 54.2f;
+            }
+         
+            if (mode == 0)
+                 tempTree = Instantiate(stepPrefab, new Vector3(-70F, tempStepYAxisPosition, (i * 40) - 35), Quaternion.identity);
+            else
+                 tempTree = Instantiate(hrPrefab, new Vector3(-70F, tempStepYAxisPosition, (i * 40) - 35), Quaternion.identity);
+
+            tempTree.transform.localScale = new Vector3(tempStepScale, tempStepScale, tempStepScale);
+        }
+
+    }
+
+}
